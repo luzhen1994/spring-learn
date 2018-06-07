49bf8e7bcc9e6d60514b21341cc0c50d3a13aa34
azhen <luzhen19940602@163.com>
2018-06-07 20:38:48 +0800

    code save

---
+++ src/main/java/com/lz/spring/aop/AppConfig.java
@@ -0,0 +1,9 @@
+package com.lz.spring.aop;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@Configuration
+@EnableAspectJAutoProxy
+public class AppConfig {
+}
